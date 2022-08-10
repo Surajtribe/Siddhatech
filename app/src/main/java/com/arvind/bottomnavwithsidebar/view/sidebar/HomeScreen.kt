e58323f303f3065a2a617e43a834ac891197297e
vishalk-siddhatech <vishalk@siddhatech.com>
2022-08-10 17:33:08 +0530

    Final Commit

--- app/src/main/java/com/arvind/bottomnavwithsidebar/view/sidebar/HomeScreen.kt
+++ app/src/main/java/com/arvind/bottomnavwithsidebar/view/sidebar/HomeScreen.kt
@@ -16,11 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
 import androidx.compose.ui.viewinterop.AndroidView
 import com.arvind.bottomnavwithsidebar.navigation.Screens
 import com.arvind.bottomnavwithsidebar.viewmodel.MainViewModel
