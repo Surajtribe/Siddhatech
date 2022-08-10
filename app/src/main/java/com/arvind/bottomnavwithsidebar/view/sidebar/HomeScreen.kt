2460123803f61bb43011060bcac33372990ab9fc
vishalk-siddhatech <vishalk@siddhatech.com>
2022-08-10 18:22:41 +0530

    Initial Commit

--- app/src/main/java/com/arvind/bottomnavwithsidebar/view/sidebar/HomeScreen.kt
+++ app/src/main/java/com/arvind/bottomnavwithsidebar/view/sidebar/HomeScreen.kt
@@ -16,9 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-
 import androidx.compose.ui.tooling.preview.Preview
-
 import androidx.compose.ui.viewinterop.AndroidView
 import com.arvind.bottomnavwithsidebar.navigation.Screens
 import com.arvind.bottomnavwithsidebar.viewmodel.MainViewModel
